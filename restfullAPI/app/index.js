31728f01219c18a2531d7bb52cab9bb7d898d3a7
Queen King <king@Queens-iMac.local>
2021-03-09 20:58:38 +0200

    http-basics

--- restfullAPI/app/index.js
+++ restfullAPI/app/index.js
@@ -1,6 +1,8 @@
 
-var http = require('http')
-var url = require('url')
+let http = require('http');
+let url = require('url');
+
+const { StringDecoder } = require('string_decoder');
 
 const server = http.createServer(function (req, res) {
 
@@ -8,15 +10,78 @@ const server = http.createServer(function (req, res) {
 
   const path = parsedUrl.pathname
 
-  var trimmedpath = path.replace(/^\/+|\/+$/g, '');
+  let queryStringObject = parsedUrl.query
 
-  res.end('Helloooooo')
+  let trimmedpath = path.replace(/^\/+|\/+$/g, '');
 
-  console.log(`Request recieved at path: ${trimmedpath}`)
+  const method = req.method.toLowerCase()
 
+  let headers = req.headers
+
+  let decoder = new StringDecoder('utf-8')
+
+  let buffer = ''
+
+  req.on('data', function (data) {
+    buffer += decoder.write(data)
+  })
+  req.on('end', () => {
+    buffer += decoder.end()
+
+    console.log(router[trimmedpath])
+
+    let chosenHandler = typeof (router[trimmedpath]) !== 'undefined' ? router[trimmedpath] : handlers.notFound
+    //construnct the data object to send to the handler
+    let data = {
+      'trimmedpath': trimmedpath,
+      'payload': buffer,
+      'headers': headers,
+      'queryStringObject': queryStringObject,
+      'method': method
+    }
+    //route the request to the handler specified  in the router 
+    chosenHandler(data, function (statusCode, payload) {
+
+      //use the status code called back by the handler
+      statuscode = typeof (statusCode) == 'number' ? statusCode : 200
+
+      payload = typeof (payload) == 'object' ? payload : {}
+
+      //convert payload to string
+      let payloadString = JSON.stringify(payload)
+      res.setHeader('Content-Type', 'applicationCache/json')
+      res.writeHead(statusCode)
+      res.end(payloadString)
+      console.log('returning this response:', statusCode, payloadString)
+
+    })
+
+    // res.end('req.ended:', ' Hellooooo\n')
+    // res.end('Helloooooo\n')
+
+    //console.log('headers:', headers)
+    // console.log('request payload is:', buffer)
+    //console.log(`Request recieved at path: ${trimmedpath} method: ${method} and  with this query string parameters:`, queryStringObject)
+  })
 })
 
 server.listen(3000, function () {
   console.log('server is listening on port 3000')
   //œ∑´®†¥¨ˆøπåß∂ƒ©˙∆˚¬…≈ç√∫˜µ≤≥≥≥«æ˜ ˜˜˜˜∫∫˜µ≈ç√∫˜µ≤≥∞§¢£™£∞§¶•ªº
 })
+
+let handlers = {}
+handlers.samplehandler = function (data, callback) { //gets data from chosenHandler function
+  callback(406, { 'name': 'sample handler' })
+}
+handlers.testHandler = function (data, callback) { //gets data from chosenHandler function
+  callback(403, { 'name': 'my test handler ;)' })
+}
+handlers.notFound = function (data, callback) {
+  callback(404)
+}
+let router = {
+  'sample': handlers.samplehandler, //function that lives here and will be called on router[sample]
+  'test': handlers.testHandler
+}
+
